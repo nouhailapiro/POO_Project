cc568209001c307942693b1123cca881fb69816a
Dohaelm <doha.elmazouni@gmail.com>
2023-12-20 00:51:40 +0100

    terrain

--- src/gestion_terrain/Terrain.java
+++ src/gestion_terrain/Terrain.java
@@ -3,9 +3,14 @@ package gestion_terrain;
 public class Terrain {
 	public int capacité;
 	public String ville;
-	public int id_Terrain;
 	public String Type_Terrain;
 	
+	public Terrain(int capacité , String ville ,String Type_Terrain ) {
+		this.capacité=capacité;
+		this.Type_Terrain=Type_Terrain;
+		this.ville=ville;
+		
 	
+	}
 	 
 }
